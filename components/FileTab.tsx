d2c02f3e301c5ba0b59c152eba6e9c7f83b23b84
RezyJS <onlyrezystor@gmail.com>
2025-03-01 21:56:57 +0300

    Made full tab clickable instead of name only

--- components/FileTab.tsx
+++ components/FileTab.tsx
@@ -6,16 +6,17 @@ import { SquareX } from 'lucide-react'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export default function Tab({ idx, stacks, setStacks }: { idx: number, setStacks: Function, stacks: StackList }) {
   return (
-    <div className='flex justify-between items-center border rounded-md py-[6px] pl-4 gap-2'>
-      <div className='flex justify-center flex-1 cursor-pointer' 
-        onClick={() => { stacks.setCurrentFile(idx); setStacks(new StackList(stacks.getFiles(), idx))}}
-      >
+    <div 
+      className='flex justify-between items-center border rounded-md py-[6px] pl-4 gap-2 cursor-pointer'
+      onClick={() => { stacks.setCurrentFile(idx); setStacks(new StackList(stacks.getFiles(), idx))}}  
+    >
+      <div className='flex justify-center flex-1'>
         <p>File {idx}</p>
       </div>
       <div className='ml-2 flex h-full'>
         <div className='h-full w-[1px] bg-white'></div>
         <SquareX className='mx-2 hover:text-red-600 cursor-pointer' onClick={
-          () => { stacks.delFile(idx); setStacks(new StackList(stacks.getFiles())); }
+          (e) => { e.stopPropagation(); stacks.delFile(idx); setStacks(new StackList(stacks.getFiles())); }
         } />
       </div>
     </div>
