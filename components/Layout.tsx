42cb0368b0e5f541abfbe9407420473e0690840c
RezyJS <onlyrezystor@gmail.com>
2025-03-02 20:15:30 +0300

    removed padding so it will now be fullscreen (without scrolling) everywhere and not only in zen

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col max-w-[100vw] w-[100vw] min-w-[100vw] max-h-[100vh] h-[100vh] min-h-[100vh] p-3">
+    <div className="flex flex-col justify-center items-center max-w-[100vw] w-[100vw] min-w-[100vw] max-h-[100vh] h-[100vh] min-h-[100vh]">
       {children}
     </div>
   )
