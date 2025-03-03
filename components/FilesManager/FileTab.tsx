d4434408adb05739d08f4abf4831dee65bd26e33
RezyJS <onlyrezystor@gmail.com>
2025-03-03 11:45:24 +0300

    temp remove

--- components/FilesManager/FileTab.tsx
+++ components/FilesManager/FileTab.tsx
@@ -1,29 +1,30 @@
 'use client'
 
 import FilesContainer from '@/lib/structures';
-import { SquareX } from 'lucide-react'
+// import { SquareX } from 'lucide-react'
 
-export default function Tab({ idx, files }: { idx: number, files: FilesContainer }) {
+export default function Tab({  }: { idx: number, files: FilesContainer }) {
   return (
-    <div
-      className='flex justify-between items-center border rounded-md py-[6px] pl-4 gap-2 cursor-pointer'
-      onClick={
-        () => {
-          files.setCurrentFile(idx);
-        }}
-    >
-      <div className='flex justify-center flex-1'>
-        <p>File {idx}</p>
-      </div>
-      <div className='ml-2 flex h-full'>
-        <div className='h-full w-[1px] bg-white'></div>
-        <SquareX className='mx-2 hover:text-red-600 cursor-pointer' onClick={
-          (e) => {
-            e.stopPropagation();
-            files.delFile(idx);
-          }
-        } />
-      </div>
-    </div>
+    // <div
+    //   className='flex justify-between items-center border rounded-md py-[6px] pl-4 gap-2 cursor-pointer'
+    //   onClick={
+    //     () => {
+    //       files.setCurrentFile(idx);
+    //     }}
+    // >
+    //   <div className='flex justify-center flex-1'>
+    //     <p>File {idx}</p>
+    //   </div>
+    //   <div className='ml-2 flex h-full'>
+    //     <div className='h-full w-[1px] bg-white'></div>
+    //     <SquareX className='mx-2 hover:text-red-600 cursor-pointer' onClick={
+    //       (e) => {
+    //         e.stopPropagation();
+    //         files.delFile(idx);
+    //       }
+    //     } />
+    //   </div>
+    // </div>
+    <div></div>
   );
 }
--- components/FilesManager/FilesSlider.tsx
+++ components/FilesManager/FilesSlider.tsx
@@ -1,22 +1,25 @@
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
-import Tab from '@/components/FilesManager/FileTab'
+// import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+// import Tab from '@/components/FilesManager/FileTab'
 import FilesContainer from '@/lib/structures';
 
-export default function FilesSlider({ files }: { files: FilesContainer }) {
+export default function FilesSlider({ }: { files: FilesContainer }) {
   return (
-    <ScrollArea className='border-white border p-2 h-14 rounded-lg whitespace-nowrap max-w-[90vw] w-full'>
-      <div className='flex gap-4'>
-        {
-          files.getFiles().map(({ id }, idx) => {
-            return (
-              <div key={idx} className='text-white'>
-                <Tab idx={id} files={files} />
-              </div>
-            );
-          })
-        }
-      </div>
-      <ScrollBar orientation='horizontal' />
-    </ScrollArea>
+    // <ScrollArea className='border-white border p-2 h-14 rounded-lg whitespace-nowrap max-w-[90vw] w-full'>
+    //   <div className='flex gap-4'>
+    //     {
+    //       files.getFiles().map(({ id }, idx) => {
+    //         return (
+    //           <div key={idx} className='text-white'>
+    //             <Tab idx={id} files={files} />
+    //           </div>
+    //         );
+    //       })
+    //     }
+    //   </div>
+    //   <ScrollBar orientation='horizontal' />
+    // </ScrollArea>
+    <div>
+
+    </div>
   )
 }
