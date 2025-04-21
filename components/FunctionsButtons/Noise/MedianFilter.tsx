504ddad8e3a6e86818e34b2ab4dcde80db00f96d
RezyJS <onlyrezystor@gmail.com>
2025-04-21 19:30:13 +0300

    fixed wrong text indication on buttons of median filter

--- components/FunctionsButtons/Noise/MedianFilter.tsx
+++ components/FunctionsButtons/Noise/MedianFilter.tsx
@@ -3,11 +3,11 @@ import { makeMedianFilter } from "@/lib/photosNoise";
 import FileElement from "@/lib/structures";
 import { MyButtonWithPopover } from "../Color/Buttons";
 
-const MedianButton = ({ num, file }: { num: number, file: FileElement }) => (
+const MedianButton = ({ text, num, file }: { text: number, num: number, file: FileElement }) => (
   <Button
     onClick={() => makeMedianFilter(num, file)}
   >
-    {num}x{num}
+    {text}x{text}
   </Button>
 )
 
@@ -15,10 +15,10 @@ export const MedianFilter = ({ file }: { file: FileElement }) => {
   return (
     <div>
       <MyButtonWithPopover text={"Median filter"}>
-        <MedianButton num={1} file={file} />
-        <MedianButton num={2} file={file} />
-        <MedianButton num={3} file={file} />
-        <MedianButton num={4} file={file} />
+        <MedianButton text={3} num={1} file={file} />
+        <MedianButton text={5} num={2} file={file} />
+        <MedianButton text={7} num={3} file={file} />
+        <MedianButton text={9} num={4} file={file} />
       </MyButtonWithPopover>
     </div>
   );
