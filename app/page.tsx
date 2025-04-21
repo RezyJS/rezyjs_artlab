2399167ae957a62fd6fcc4de6945799f88ff0805
RezyJS <onlyrezystor@gmail.com>
2025-04-21 17:54:27 +0300

    Added hotkeys

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 'use client'
 
 import Container from "@/components/ResizableContainer/ResizableContainer";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import FileElement from "@/lib/structures";
+import { useHotkeys } from 'react-hotkeys-hook'
 
 export default function App() {
 
   const [picture, setPicture] = useState<string>('/placeholder.jpg');
   const [file,] = useState<FileElement>(new FileElement(setPicture));
+
+  const handleUndo = useCallback(() => {
+    file.revert();
+  }, [file]);
+
+  const handleRedo = useCallback(() => {
+    file.undoRevert();
+  }, [file])
+
+  const handleReset = useCallback(() => {
+    file.reset();
+  }, [file])
+
+  const handleDelete = useCallback(() => {
+    file.newStack();
+  }, [file])
+
+  useHotkeys('ctrl+z', handleUndo, [handleUndo]);
+  useHotkeys('ctrl+y', handleRedo, [handleRedo]);
+  useHotkeys('ctrl+x', handleReset, [handleReset]);
+  useHotkeys('ctrl+m', handleDelete, [handleDelete]);
 
   if (file === undefined) {
     return (
--- components/ResizableContainer/ControlPhoto.tsx
+++ components/ResizableContainer/ControlPhoto.tsx
@@ -5,39 +5,55 @@ import { ArrowLeftCircle, ArrowRightCircle, RotateCcw, Trash2 } from "lucide-rea
 export const ControlPhoto = ({ file }: { file: FileElement }) => {
   return (
     <div className="flex justify-evenly items-center gap-3">
-      <Button
-        disabled={file.isEmpty() || file.isFirst()}
-        onClick={() => {
-          file.revert()
-        }}
-        className="flex flex-row w-full justify-between gap-5"
-      >
-        <ArrowLeftCircle />
-      </Button>
-      <Button disabled={file.isEmpty() || file.isLast()}
-        onClick={() => {
-          file.undoRevert()
-        }}
-        className="flex flex-row w-full justify-between gap-5"
-      >
-        <ArrowRightCircle />
-      </Button>
-      <Button disabled={file.isEmpty() || file.isFirst()}
-        onClick={() => {
-          file.reset()
-        }}
-        className="flex flex-row w-full justify-between gap-5"
-      >
-        <RotateCcw />
-      </Button>
-      <Button disabled={file.isEmpty()}
-        onClick={() => {
-          file.newStack();
-        }}
-        className="flex flex-row w-full justify-between gap-5"
-      >
-        <Trash2 />
-      </Button>
+      <div className="flex flex-col text-center w-full">
+        Undo
+        <Button
+          disabled={file.isEmpty() || file.isFirst()}
+          onClick={() => {
+            file.revert()
+          }}
+          className="flex flex-col h-full w-full justify-center items-center"
+        >
+          <ArrowLeftCircle />
+          Ctrl+Z
+        </Button>
+      </div>
+      <div className="flex flex-col text-center w-full">
+        Redo
+        <Button disabled={file.isEmpty() || file.isLast()}
+          onClick={() => {
+            file.undoRevert()
+          }}
+          className="flex flex-col h-full w-full justify-center items-center"
+        >
+          <ArrowRightCircle />
+          Ctrl+Y
+        </Button>
+      </div>
+      <div className="flex flex-col text-center w-full">
+        Reset
+        <Button disabled={file.isEmpty() || file.isFirst()}
+          onClick={() => {
+            file.reset()
+          }}
+          className="flex flex-col h-full w-full justify-center items-center"
+        >
+          <RotateCcw />
+          Ctrl+X
+        </Button>
+      </div>
+      <div className="flex flex-col text-center w-full">
+        Delete
+        <Button disabled={file.isEmpty()}
+          onClick={() => {
+            file.newStack();
+          }}
+          className="flex flex-col h-full w-full justify-center items-center"
+        >
+          <Trash2 />
+          Ctrl+M
+        </Button>
+      </div>
     </div>
   );
 }
